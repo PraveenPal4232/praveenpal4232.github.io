98f18d1037eb0633ec51455c28b1a5457e265d5a
Praveen Pal <praveenpal4232@gmail.com>
2019-12-15 16:05:14 +0530

    added Service Worker

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
