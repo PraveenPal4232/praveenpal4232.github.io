39f16ab651728c6e04385a4ea7b5f13c15f2cf5b
Praveen Pal <praveenpal4232@gmail.com>
2019-12-15 16:23:25 +0530

    update

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
